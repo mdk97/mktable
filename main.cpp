aee16ea095ed6d62543a98c5c3f02ea0b0557afc
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-09-23 14:56:10 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,31 @@
 
 int main(int argc, char *argv[])
 {
-    
+    file fh;
+    string file_name = GetFileNameFromArgv(argv);
+    try
+    {
+        fh = OpenFile(file_name);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    uint column_size = GetLongestStringInFile(fh, ' ');
+    CloseFile(fh);
+
+
+    try
+    {
+        fh = OpenFile(file_name);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    WriteTable(fh, ' ', column_size);
+
+    CloseFile(fh);
 }
