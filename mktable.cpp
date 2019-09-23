24b6be3071b5da0d241de2f7cb9997d46ec55eb1
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-09-23 14:56:41 -0300

    Update mktable.cpp

--- mktable.cpp
+++ mktable.cpp
@@ -1,5 +1,8 @@
 #include "mktable.hpp"
 
+using std::cout;
+using std::endl;
+
 file OpenFile(string file_name)
 {
     file file_handler;
@@ -56,21 +59,80 @@ vector<string> StringSplit(string str, char delimiter)
     return(split_string);
 }
 
-string GenerateHeader(string header_line)
+uint GetLongestStringInFile(file &file_handler, char delimiter)
+{
+    if (not file_handler.is_open())
+    {
+        throw("Read error: file wasn't open");
+        return(0);
+    }
+
+    uint longest = 0;
+    while (not file_handler.eof())
+    {
+        string temp;
+        file_handler >> temp;
+        vector<string> line = StringSplit(temp, delimiter);
+        for (string s : line)
+        {
+            if (s.length() > longest)
+                longest = s.length();
+        }
+    }
+    return(longest);
+}
+
+string GenerateDivision(string header_line, uint column_size)
 {
     string header("+");
-    for (int i = 1; i < COLUMN_SIZE; i++)
-        header.append("-");
-    header.append("+");
+    for (int i = 1; i < column_size * GetColumnCount(header_line); i++)
+    {
+        if (i % column_size != 0)
+            header.push_back('-');
+        else
+            header.push_back('+');
+
+    }
+    header.push_back('+');
     return(header);
 }
 
-string GenerateLine(vector<string> line, uint columns)
+string GenerateLine(vector<string> line, uint columns, uint column_size)
 {
     string formatted_line("|");
     for (uint i = 0; i < columns; i++)
     {
         formatted_line.append(line[i]);
-
+        for (auto j = line[i].length(); j < column_size; j++)
+        {
+            formatted_line.push_back(' ');
+        }
+        formatted_line.push_back('|');
     }
+    return(formatted_line);
+}
+
+void WriteTable(file &file_handler, char delimiter, uint column_size)
+{
+    string line = ReadLineFromFile(file_handler);
+    string division = GenerateDivision(line, column_size);
+    uint columns = GetColumnCount(line);
+
+    cout << division << endl;
+    cout << GenerateLine(StringSplit(line, delimiter), columns, column_size);
+
+    while (true)
+    {
+        cout << division << endl;
+        line = ReadLineFromFile(file_handler);
+        if (file_handler.eof())
+            return;
+        cout << GenerateLine(StringSplit(line, delimiter), columns, column_size);
+    }
+}
+
+string GetFileNameFromArgv(char *argv[])
+{
+    string r(argv[1]);
+    return(r);
 }
