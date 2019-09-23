b2a5128f403007fe57d9b7ba724c0660e6bc6720
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-09-23 11:23:07 -0300

    Update mktable.hpp

--- mktable.hpp
+++ mktable.hpp
@@ -1,18 +1,23 @@
-#pragme once
+#pragma once
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using std::string;
 using file = std::fstream;
+using std::vector;
+
+extern const auto COLUMN_SIZE;
 
 string GenerateHeader(string header_line);
-string GenerateLine(string line, uint columns);
+string GenerateLine(vector<string> line, uint columns);
 string GenerateFooter();
 
 string ReadLineFromFile(file &file_handler);
 file OpenFile(string file_name);
-bool CloseFile(string &file_handler);
+bool CloseFile(file &file_handler);
 
-uint ParseFile(string header_line);
+uint GetColumnCount(string header_line);
+vector<string> StringSplit(string str, char delimiter);
