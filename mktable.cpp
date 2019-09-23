51f7abcb2f211f9450923e4e5b5391f782276231
Bruno Sacconi Peres <32146278+mdk97@users.noreply.github.com>
2019-09-23 10:58:52 -0300

    Update mktable.cpp

--- mktable.cpp
+++ mktable.cpp
@@ -1,1 +1,54 @@
 #include "mktable.hpp"
+
+file OpenFile(string file_name)
+{
+    file file_handler;
+    file_handler.open(file_name, file::in);
+    return(file_handler);
+}
+
+bool CloseFile(string &file_handler);
+{
+    if (file_handler.is_open())
+        file_handler.close();
+    else
+        return(false);
+    return(true);
+}
+
+string ReadLineFromFile(file &file_handler)
+{
+    if (not file_handler.is_open())
+    {
+        throw ("File is not open");
+        return("");
+    }
+    string line;
+    line << file_handler;
+    return(line);
+}
+
+uint ParseFile(string header_line)
+{
+    uint counter = 0;
+    for (char a : header_line)
+    {
+       if (a == ' ')
+           counter++;
+    }
+    return(counter);
+}
+
+string GenerateHeader(string header_line)
+{
+    string header("+");
+    for (int i = 1; i < header_line.length(); i++)
+        header.append("-");
+    header.append("+");
+    return(header);
+}
+
+string GenerateLine(string line, uint columns)
+{
+    string formatted_line("|");
+}
